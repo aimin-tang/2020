95677d1f6e36fd717f2d032eea3194cd145b0f38
Aimin Tang <aimin.tang@gmail.com>
2020-05-06 06:21:58 -0500

    fake nextMove()

--- playground/utils/solve.js
+++ playground/utils/solve.js
@@ -1,12 +1,21 @@
+let nextMove = function (g) {
+    return {
+        row: 0,
+        col: 0,
+        num: 1
+    }
+}
+
 let move1 = function (g) {
-    g[0] = "111111111";
+    let { row, col, num } = nextMove(g);
+    g[row] = g[row].substr(0, col) + num + g[row].substr(col + 1);
 }
 
 let moveAll = function (g) {
     g[1] = "111111111";
 }
 
-export  {
+export {
     move1,
     moveAll
 }
