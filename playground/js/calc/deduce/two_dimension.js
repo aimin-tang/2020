7ee821bda96363905029da892159a47ecec9d2f5
Aimin Tang <aimin.tang@gmail.com>
2020-05-09 06:24:27 -0500

    import basic

--- playground/js/calc/deduce/two_dimension.js
+++ playground/js/calc/deduce/two_dimension.js
@@ -1,4 +1,4 @@
-import { countUnknown, arrToStr, strToArr, boxStartRow, boxStartCol, boxStarts } from '../../lib/basic.js';
+import * as basic from '../../lib/basic.js';
 
 let solveIn2d = function (g, d1Idx, d2Idx, num) {
 
